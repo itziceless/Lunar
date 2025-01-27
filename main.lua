806f2af88b9bbe7f0f11ecbfa302bcab7982968f
Iceless <xiceless@gmail.com>
2025-01-27 18:39:19 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -12,7 +12,7 @@ local vape
 local loadstring = function(...)
 	local res, err = loadstring(...)
 	if err and vape then
-		vape:CreateNotification('Vape', 'Failed to load : '..err, 30, 'alert')
+		vape:CreateNotification('Lunar', 'Failed to load : '..err, 30, 'alert')
 	end
 	return res
 end
