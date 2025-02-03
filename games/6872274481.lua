4becb1b1aa17e1e9007b7ff9547a2550c4baf0d1
Iceless <xiceless@gmail.com>
2025-02-03 15:48:55 -0500

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -7225,75 +7225,7 @@ run(function()
 		Name = 'Limit to items',
 		Tooltip = 'Only breaks when tools are held'
 	})
-end)
-
-run(function()
-	local Desync
-	local Type
-	local AutoSend
-	local AutoSendLength
-	local NoFly
-	local oldphys, oldsend
-	
-	Desync = vape.Categories.Minigames:CreateModule({
-		Name = 'Desync',
-		Function = function(callback)
-			if callback then
-				local teleported
-				Desync:Clean(lplr.OnTeleport:Connect(function()
-					setfflag('S2PhysicsSenderRate', '15')
-					setfflag('DataSenderRate', '60')
-					teleported = true
-				end))
-	
-				repeat
-					local physicsrate, senderrate = '0', Type.Value == 'All' and '-1' or '60'
-					if (vape.Modules.Fly.Enabled or vape.Modules.InfiniteFly.Enabled) and NoFly.Enabled then
-						setfflag('S2PhysicsSenderRate', '15')
-						setfflag('DataSenderRate', '60')
-						oldphys, oldsend = nil, nil
-					else
-						if tick() % (AutoSendLength.Value + 0.1) > AutoSendLength.Value then
-							physicsrate, senderrate = '15', '60'
-						end
-		
-						if physicsrate ~= oldphys or senderrate ~= oldsend then
-							setfflag('S2PhysicsSenderRate', physicsrate)
-							setfflag('DataSenderRate', senderrate)
-							oldphys, oldsend = physicsrate, oldsend
-						end
-					end
-					
-					task.wait(0.03)
-				until (not Desync.Enabled and not teleported)
-			else
-				setfflag('S2PhysicsSenderRate', '15')
-				setfflag('DataSenderRate', '60')
-				oldphys, oldsend = nil, nil
-			end
-		end,
-		Tooltip = 'Desyncs ur character.\n (basically looks like your really laggy)'
-	})
-	Type = Desync:CreateDropdown({
-		Name = 'Type',
-		List = {'Movement Only', 'All'},
-		Tooltip = 'Movement Only - Only chokes movement packets\nAll - Chokes remotes & movement'
-	})
-	AutoSendLength = Desync:CreateSlider({
-		Name = 'Send delay',
-		Min = 0,
-		Max = 1,
-		Decimal = 100,
-		Suffix = function(val)
-			return val == 1 and 'second' or 'seconds'
-		end
-	})
-	NoFly = Desync:CreateToggle({
-		Name = 'Disable when flying',
-		Default = true
-	})
-end)
-											
+end)										
 	
 run(function()
 	vape.Legit:CreateModule({
@@ -8343,6 +8275,73 @@ run(function()
 	List = WinEffect:CreateDropdown({
 		Name = 'Effects',
 		List = WinEffectName
+	})
+end)
+
+run(function()
+	local Desync
+	local Type
+	local AutoSend
+	local AutoSendLength
+	local NoFly
+	local oldphys, oldsend
+	
+	Desync = vape.Categories.FFlags:CreateModule({
+		Name = 'Desync',
+		Function = function(callback)
+			if callback then
+				local teleported
+				Desync:Clean(lplr.OnTeleport:Connect(function()
+					setfflag('S2PhysicsSenderRate', '15')
+					setfflag('DataSenderRate', '60')
+					teleported = true
+				end))
+	
+				repeat
+					local physicsrate, senderrate = '0', Type.Value == 'All' and '-1' or '60'
+					if (vape.Modules.Fly.Enabled or vape.Modules.InfiniteFly.Enabled) and NoFly.Enabled then
+						setfflag('S2PhysicsSenderRate', '15')
+						setfflag('DataSenderRate', '60')
+						oldphys, oldsend = nil, nil
+					else
+						if tick() % (AutoSendLength.Value + 0.1) > AutoSendLength.Value then
+							physicsrate, senderrate = '15', '60'
+						end
+		
+						if physicsrate ~= oldphys or senderrate ~= oldsend then
+							setfflag('S2PhysicsSenderRate', physicsrate)
+							setfflag('DataSenderRate', senderrate)
+							oldphys, oldsend = physicsrate, oldsend
+						end
+					end
+					
+					task.wait(0.03)
+				until (not Desync.Enabled and not teleported)
+			else
+				setfflag('S2PhysicsSenderRate', '15')
+				setfflag('DataSenderRate', '60')
+				oldphys, oldsend = nil, nil
+			end
+		end,
+		Tooltip = 'Desyncs ur character.\n (basically looks like your really laggy)'
+	})
+	Type = Desync:CreateDropdown({
+		Name = 'Type',
+		List = {'Movement Only', 'All'},
+		Tooltip = 'Movement Only - Only chokes movement packets\nAll - Chokes remotes & movement'
+	})
+	AutoSendLength = Desync:CreateSlider({
+		Name = 'Send delay',
+		Min = 0,
+		Max = 1,
+		Decimal = 100,
+		Suffix = function(val)
+			return val == 1 and 'second' or 'seconds'
+		end
+	})
+	NoFly = Desync:CreateToggle({
+		Name = 'Disable when flying',
+		Default = true
 	})
 end)
 
