8cc952ddaaad8488720db63313a679d0dde2bf47
Iceless <xiceless@gmail.com>
2025-02-22 17:08:45 -0500

    Update new.lua

--- guis/new.lua
+++ guis/new.lua
@@ -5993,7 +5993,7 @@ scaleslider = guipane:CreateSlider({
 })
 guipane:CreateDropdown({
 	Name = 'GUI Theme',
-	List = inputService.TouchEnabled and {'new', 'old'} or {'new', 'old', 'rise', 'thunder'},
+	List = inputService.TouchEnabled and {'new', 'old'} or {'new', 'old', 'rise', 'th'},
 	Function = function(val, mouse)
 		if mouse then
 			writefile('newvape/profiles/gui.txt', val)
